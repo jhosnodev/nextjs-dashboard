3f72bd2ca424fd8cbd1b9425aa2471328b3f9571
jhosno <jhosno.dev@gmail.com>
2024-01-06 19:49:17 -0400

    Base de nextauth

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+
+import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] });
+export const poppins = Poppins({ weight: ['400', '700'], subsets: ['latin'] }); 
